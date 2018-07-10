3f4c5dc6c263f877d35b2744d04742e240c2fee6
Emil Svensson <svempa@Emils-MacBook-Pro.local>
2018-07-10 15:02:06 +0200

    Missed error log

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -16,6 +16,7 @@ function setTimeZone {
 touch /var/log/fail2ban.log
 touch /var/log/auth.log
 touch /var/log/apache2/access.log
+touch /var/log/apache2/error.log
 setTimeZone
 service fail2ban stop
 rm -f /var/run/fail2ban/*
