cab803ab40d9fe815f5f958b7c4c982fb41e12a4
Emil Svensson <svenne87@gmail.com>
2019-09-17 15:25:55 +0200

    Read the whitelist from config file

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -15,13 +15,27 @@ function setTimeZone {
 
 touch /var/log/fail2ban.log
 touch /var/log/auth.log
+
 mkdir -p /var/log/apache2/ && touch /var/log/apache2/access.log
 mkdir -p /var/log/apache2/ && touch /var/log/apache2/error.log
 mkdir -p /var/log/mysql/ && touch /var/log/mysql/error.log
+
 setTimeZone
 service fail2ban stop
+
 rm -f /var/run/fail2ban/*
-docker=$(ip addr|awk '/docker/ && /inet/ {gsub(/\/[0-9][0-9]/,""); print $2}'); sed -i "/^ignoreip =/ s/$/ ${docker}\/8/" /etc/fail2ban/jail.local
-bridge=$(ip addr|awk '/br-/ && /inet/ {gsub(/\/[0-9][0-9]/,""); print $2}'); sed -i "/^ignoreip =/ s/$/ ${bridge}\/8/" /etc/fail2ban/jail.local
+
+docker=$(ip addr|awk '/docker/ && /inet/ {gsub(/\/[0-9][0-9]/,""); print $2}');
+
+if ! grep -q "$docker" /etc/fail2ban/whitelist.conf; then
+    sed -i "/^ignoreip =/ s/$/ ${docker}\/8/" /etc/fail2ban/whitelist.conf
+fi
+
+bridge=$(ip addr|awk '/br-/ && /inet/ {gsub(/\/[0-9][0-9]/,""); print $2}');
+
+if ! grep -q "$bridge" /etc/fail2ban/whitelist.conf; then
+    sed -i "/^ignoreip =/ s/$/ ${bridge}\/8/" /etc/fail2ban/whitelist.conf
+fi
+
 service fail2ban start
 tailf /var/log/fail2ban.log
